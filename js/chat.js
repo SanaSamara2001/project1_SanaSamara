98d424c28b970ad191077b41340078b307f6f8b8
SanaSamara2001 <sanasamarakote@gmail.com>
2025-06-15 22:18:57 +0300

    Initial commit

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "4a1b9837-636a-455a-a991-84a5bf3bbb1e";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
